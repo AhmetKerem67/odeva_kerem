667e602b3a345150894ebe1c4376d9ff1aaab54e
AhmetKerem67 <108231290+AhmetKerem67@users.noreply.github.com>
2022-06-26 19:26:59 +0300

    Add files via upload

---
+++ kerem.py
@@ -0,0 +1,68 @@
+#soru 1
+"""
+ad = input("öğrencinin adı : ")
+birinci = int(input("birinci sınav notu : "))
+ikinci = int(input("ikinci sınav notu : "))
+ort = (birinci+ikinci)/2
+print(f"öğrencinin adı : {ad} sınav ortalaması : {ort}")
+"""
+
+#soru 2
+"""
+r = int(input("dairenin yarıçapını girin : "))
+alan = (r**2)*3.14
+cevre = 2*3.14*r
+print(f"dairenin alanı = {alan},dairenin çevresi = {cevre}")
+"""
+
+#soru 3
+
+url = "https://teknolojiaihl.meb.k12.tr/"
+okul  = "Teknoloji Anadolu İmam Hatip Lisesi"
+"""
+uzunluk = len(url)
+print(f"url değişkeni içinde : {uzunluk}  karakter bulunmaktadır")
+"""
+
+#soru 4
+"""
+okul=okul.upper()
+okul=okul.replace("I","İ")
+print(okul)
+"""
+
+#soru 5
+
+"""
+print(okul.find('T'))
+"""
+
+#soru 6
+
+"""
+print(okul.isalpha())
+"""
+
+#soru 7
+
+"""
+print(okul.isdigit())
+"""
+
+#soru 8
+
+"""
+print(okul.startswith('T'))
+"""
+
+#soru 9
+
+"""
+print(okul.endswith('T'))
+"""
+
+#soru 10
+
+"""
+print(okul.replace("T","K"))
+"""
